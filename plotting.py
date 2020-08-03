d7565edcb42fddcc1cc1e37cb41bdfd1a6892b72
IritaSee <iga.narendra@gmail.com>
2020-08-03 15:53:20 +0800

    add colour scatter plot and line

--- plotting.py
+++ plotting.py
@@ -6,3 +6,7 @@ plt.scatter(df['Age'],df['Fare'])
 #format si scatter itu akan di plot scatter (x,y)
 plt.xlabel('Age');plt.ylabel('Fare')
 #Kasih nama biar ga bingung
+plt.scatter(df['Age'],df['Fare'],c=df['Pclass'])
+#c itu ngasih warna
+plt.plot([0, 80], [85, 5])
+#ngasih garis dari x1 y1 ke x2 y2
